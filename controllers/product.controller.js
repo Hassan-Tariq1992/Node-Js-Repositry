a9d716b683554438861b5b57aee3ce1bf31aeae6
Hassan-Tariq1992 <hassantariq9933@yahoo.com>
2019-01-16 12:12:49 +0500

    Update product.controller.js

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,4 +1,4 @@
-const Product = require('~/models/product.model');
+const Product = require('../models/product.model');
 exports.test=function(req,res){
 	res.send('Greeting from Test Controller!');
 	
